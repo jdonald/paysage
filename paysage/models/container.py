bc94f08fb4f34d7f98ed319555cb056dbdc783cb
Charles Fisher <charlesfisher@Charless-MacBook-Pro.local>
2017-02-02 07:29:56 -0800

    skeleton

---
+++ paysage/models/container.py
@@ -0,0 +1,18 @@
+import pandas, json, h5py
+
+class Model(object):
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def get_config(self):
+        pass
+
+    def from_config(self, config):
+        pass
+
+    def save(self):
+        pass
+
+    def read(self, filename):
+        pass
